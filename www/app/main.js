eb4b4fe9c0867a783b0359446907ca52a9129652
Amr Yousef <amr.yousef@dashsoft.com>
2014-07-06 11:08:01 +0200

    no message

--- www/app/main.js
+++ www/app/main.js
@@ -12,7 +12,7 @@
         'bootstrap': {
             deps: ['jquery'],
             exports: 'jQuery'
-        }
+       }
     }
 });
 
@@ -21,19 +21,19 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
     system.debug(true);
     //>>excludeEnd("build");
 
-    app.title = 'Durandal Samples';
+    app.title = 'Durandal Starter Kit';
 
-    //specify which plugins to install and their configuration
     app.configurePlugins({
         router:true,
-        dialog: true,
-        widget: {
-            kinds: ['expander']
-        }
+        dialog: true
     });
 
-    app.start().then(function () {
+    app.start().then(function() {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
         viewLocator.useConvention();
-        app.setRoot('shell');
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
     });
 });
